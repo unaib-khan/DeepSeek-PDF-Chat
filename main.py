1daa59cac7d30e99492c1a9f81bb37bc7e4eea30
Unaib khan <unaibk07@gmail.com>
2025-01-29 22:05:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
-st.secrets("GROQ_API_KEY")
+secrets= st.secrets("GROQ_API_KEY")
 
 # Load Sentence Transformer model
 embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
